31958870a445b2e1d3675e1613d2470ce6132867
unknown <promustafataha@gmail.com>
2020-05-02 23:28:46 +0200

    page main content

--- native-html-css-javascipt-version/assets/js/script.js
+++ native-html-css-javascipt-version/assets/js/script.js
@@ -1,3 +1,7 @@
+let tabs = document.querySelector('.tabs');
+let tabButtons = tabs.querySelectorAll('[role="tab"]');
+let tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+
 document.onload = loadinAnimaion();
 
 function loadinAnimaion() {
@@ -17,3 +21,27 @@ function loadinAnimaion() {
         }
     }, 250);
 }
+
+
+
+function handleTabClick(event) {
+    //hide all tab panels
+    tabPanels.forEach(function (panel) {
+        panel.hidden = true
+    })
+
+    //mark all tabs as un selected
+    tabButtons.forEach(button => {
+        button.setAttribute('aria-selected', 'false')
+    })
+    //mark the clicked tab as selected
+    event.currentTarget.setAttribute('aria-selected', 'true')
+
+    //find the associated tab panel an show it
+    let id = event.currentTarget.id;
+
+    let tabPanel = tabPanels.find(panel => panel.getAttribute('aria-labelledby') === id);
+    tabPanel.hidden = false;
+}
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick))
